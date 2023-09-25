9ebe3089ddefa241be50e24408ccb98d6d8b341c
gustavocarlim <gustavocarlimsaens@gmail.com>
2023-09-25 17:15:46 -0300

    req 20 até o 24

--- desafio20.sql
+++ desafio20.sql
@@ -1,2 +1,3 @@
-SELECT notes FROM purchase_orders
-WHERE notes IS NOT NULL;
+INSERT INTO order_details (order_id, product_id, quantity, unit_price, discount, status_id, date_allocated, purchase_order_id, inventory_id)
+VALUES
+(69, 80, 15.0000, 15.0000, 0, 2, NULL, NULL, 129);
